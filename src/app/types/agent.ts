9f37eb8c54ed5f6e4e3cccc363bac6db16c811ac
atulj10 <atulanandj10@gmail.com>
2025-02-16 23:29:34 +0530

    Type Corrected and new type created

--- src/app/types/agent.ts
+++ src/app/types/agent.ts
@@ -1,8 +1,14 @@
 import { Types } from "mongoose";
 
 export interface AgentTypes extends Document {
+  _id?:string,
   name: string;
   description: string;
   userId: Types.ObjectId;
   timestamp: Date;
+}
+
+export interface NewAgentTypes extends Document {
+  name: string;
+  description: string;
 }
