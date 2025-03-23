c994fa30018a7aa80431736ecee1a3a4ebd5c23a
atulj10 <atulanandj10@gmail.com>
2025-03-24 00:17:11 +0530

    Redirect corrected

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ export async function middleware(request: NextRequest) {
     if (token && (
         url.pathname.startsWith('/sign-in') ||
         url.pathname.startsWith('/login'))) {
-        return NextResponse.redirect(new URL('/', request.url))
+        return NextResponse.redirect(new URL('/main/dashboard', request.url))
     }
     if (!token && (
         url.pathname.startsWith('/main'))) {
